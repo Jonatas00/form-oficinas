97f372d73100318feb6d0316ff45d3635483d63d
Guilherme Wilker Santos Silva <gwlima@rainhadapaz.local>
2025-06-12 13:09:54 -0300

    refactor: mudando os textos e banner do formulário

--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -1,23 +1,33 @@
 <template>
   <header class="non-draggable pointer-events-none">
-    <img src="/banner.png" alt="Banner Rainha da Paz" class="w-full object-cover mb-4 rounded-md shadow" />
+    <img src="/cabecalho-form.png" alt="Banner Rainha da Paz" class="w-full object-cover mb-4 rounded-md shadow" />
 
     <div class="text-justify space-y-2 px-4 text-sm md:text-md lg:text-lg">
-      <h1 class="text-[--primary] text-xl font-semibold">Inscrições para oficina</h1>
+      <h1 class="text-[--primary] text-xl font-semibold">Inscrição para oficina no evento de 75 do Rainha</h1>
 
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, aliquid corrupti! Illo atque voluptates mollitia
-        libero impedit explicabo nobis fugiat vero, deserunt asperiores itaque accusantium, repudiandae corrupti maxime
-        laboriosam sit.
+        Caras professoras e professores,
       </p>
       <p>
+        Pedimos que selecionem abaixo a oficina e o horário de sua preferência para participação no dia do aniversário do Rainha (23/08). Haverá dois turnos (das 9h30 às 11h30 e das 11h30 às 13h30) e é necessário se inscrever apenas em um turno.
+
+
+        <!-- <br> -->
+
+      </p>
+      <p>
+        Quando as vagas para determinada oficina estiverem esgotadas, a inscrição ficará inabilitada. <br>
+        Prazo para inscrições: até o dia 23/06.
+        Muito obrigado!
+      </p>
+      <!-- <p>
         O evento acontecerá no <strong>dia xx de xxxxx de 2025</strong>, das <strong>xxh às xxh</strong>.
       </p>
 
       <p class="bg-neutral-50 p-3 rounded-md border text-[--primary] font-medium">
         Para isso, pedimos que escolha uma das oficinas disponíveis, de acordo com sua preferência e disponibilidade.
         Fique atento ao número de vagas por turno (manhã ou tarde), pois são limitadas.
-      </p>
+      </p> -->
     </div>
   </header>
 </template>
