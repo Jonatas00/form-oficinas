39286a5b45bbfdf9d03f7cb86383f01a53847142
Jonatas <djonatas31@gmail.com>
2025-06-12 10:41:29 -0300

    feat: add baner and minor improves

--- src/components/AlertComponent.vue
+++ src/components/AlertComponent.vue
@@ -4,20 +4,17 @@ import { CheckCircle, XCircle } from "lucide-vue-next";
 
 const props = defineProps({
   message: String,
-  statusCode: Number,
+  sucesso: Boolean,
 });
 
-const isError = props.statusCode !== 200;
+const isError = !props.sucesso;
 </script>
 
 <template>
-  <Alert
-    :class="
-      isError
-        ? 'bg-red-100 border-red-500 text-red-800'
-        : 'bg-green-100 border-green-500 text-green-800'
-    "
-  >
+  <Alert :class="isError
+      ? 'bg-red-100 border-red-500 text-red-800'
+      : 'bg-green-100 border-green-500 text-green-800'
+    ">
     <component :is="isError ? XCircle : CheckCircle" class="h-4 w-4" />
     <AlertTitle>{{ isError ? "Erro!" : "Sucesso!" }}</AlertTitle>
     <AlertDescription>
